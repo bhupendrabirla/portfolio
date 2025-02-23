4e7d3807252b13329a1f0692e593100d095ab63e
Bhupendra Birla <bhupendrabirla91@gmail.com>
2025-02-23 23:05:11 +0530

    My portfolio

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,60 @@
+import React from "react";
+import HeroBgAnimation from "../HeroBgAnimation";
+import {
+  HeroContainer,
+  HeroBg,
+  HeroLeftContainer,
+  Img,
+  HeroRightContainer,
+  HeroInnerContainer,
+  TextLoop,
+  Title,
+  Span,
+  SubTitle,
+  ResumeButton,
+} from "./HeroStyle";
+import ProfilePic from "../../images/profile-pic.jpg";
+import Typewriter from "typewriter-effect";
+import { Bio } from "../../data/constants";
+
+const HeroSection = () => {
+
+  return (
+    <div id="about">
+      <HeroContainer>
+        <HeroBg>
+          <HeroBgAnimation />
+        </HeroBg>
+        <HeroInnerContainer>
+          <HeroLeftContainer id="Left">
+            <Title>
+              Hi, I am <br /> {Bio.name}
+            </Title>
+            <TextLoop>
+              I am a
+              <Span>
+                <Typewriter
+                  options={{
+                    strings: Bio.roles,
+                    autoStart: true,
+                    loop: true,
+                  }}
+                />
+              </Span>
+            </TextLoop>
+            <SubTitle>{Bio.description}</SubTitle>
+            <ResumeButton href={Bio.resume} target="display">
+              View Resume
+            </ResumeButton>
+          </HeroLeftContainer>
+
+          <HeroRightContainer id="Right">
+            <Img src={ProfilePic} alt="hero-image" />
+          </HeroRightContainer>
+        </HeroInnerContainer>
+      </HeroContainer>
+    </div>
+  );
+};
+
+export default HeroSection;
---
+++ src/data/constants.js
@@ -0,0 +1,280 @@
+export const Bio = {
+  name: "Bhupendra Birla",
+  roles: ["Java Developer", "Software Engineer", "Programmer"],
+  description:
+    "I am a motivated and adaptable individual, always ready to embrace new challenges. Passionate about learning, I am committed to delivering high-quality results. With a positive attitude and a growth mindset, I strive to make a meaningful impact and accomplish great things.",
+  github: "https://github.com/bhupendrabirla",
+  resume:
+    "https://drive.google.com/file/d/1n56MHObk4bd6HKKXkRGN6qUPmvfMLDZe/view?usp=drive_link",
+  linkedin: "https://www.linkedin.com/in/bhupendra-birla/",
+  whatsapp: "https://wa.link/meosu3"
+};
+
+export const skills = [
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Java",
+        image:
+          "https://static-00.iconduck.com/assets.00/java-icon-1511x2048-6ikx8301.png",
+      },
+      {
+        name: "Spring Boot",
+        image: "https://image.pngaaa.com/546/2459546-middle.png",
+      },
+      {
+        name: "Kotlin",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkD0hl4RMkT1HCsSWKPEG-UqrOfJa8l7wx5w&s",
+      },
+      {
+        name: "MicroServices",
+        image:
+          "https://w1.pngwing.com/pngs/535/256/png-transparent-microservices-text-computer-software-line-circle-area-diagram-symmetry-organization-thumbnail.png",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://w7.pngwing.com/pngs/717/111/png-transparent-mysql-round-logo-tech-companies-thumbnail.png",
+      },
+      {
+        name: "Postgresql",
+        image:
+          "https://ironfocus.com/wp-content/uploads/2020/04/4-postgres.jpg",
+      },
+      {
+        name: "Redis",
+        image: "https://banner2.cleanpng.com/20180331/xwe/avh53unx5.webp",
+      },
+    ],
+  },
+  {
+    title: "Front-end",
+    skills: [
+      {
+        name: "HTML",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBplqQBq-srDlqkapHt_fNhWD3QD8DCjt1Vg&s",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTh2DuolUB6Lp4uJmajNd3v5IvXs5_KmG5OFw&s",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpTdm2oNc3NJWLwYgGheya0Z-RjVhv0bsbBw&s",
+      },
+      {
+        name: "React.js",
+        image:
+          "https://static-00.iconduck.com/assets.00/react-icon-512x512-u6e60ayf.png",
+      },
+      {
+        name: "MUI",
+        image: "https://mui.com/static/logo.png",
+      },
+    ],
+  },
+  {
+    title: "Cloud",
+    skills: [
+      {
+        name: "Docker",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+      },
+      {
+        name: "Kubernetes",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFjirfXYnG_YnbfqSHB3KKNxnpS1L5hbxF6w&s",
+      },
+      {
+        name: "Amazon S3",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3UVEC-B6F97ezOTFuAG7Hmf5UzEvWy2qk8A&s",
+      },
+      {
+        name: "Amazon SQS",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-SfYNdVYWxNYY72BTHQEupd8y6kgCODN74g&s",
+      },
+      {
+        name: "Datadog",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiAWPKY8S53sKiGowDub28lA2r13oh9RccMQ&s",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "GitLab",
+        image:
+          "https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/144_Gitlab_logo_logos-512.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "IntelliJ IDEA",
+        image:
+          "https://www.helenjoscott.com/wp-content/uploads/2020/09/1200px-IntelliJ_IDEA_Logo.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Maven",
+        image:
+          "https://e7.pngegg.com/pngimages/917/651/png-clipart-apache-maven-feathers-tech-companies.png",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://cdn.iconscout.com/icon/free/png-256/free-postman-logo-icon-download-in-svg-png-gif-file-formats--technology-social-media-company-brand-vol-5-pack-logos-icons-2945092.png?f=webp&w=256",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://cdnv2.cutshort.io/company-static/5baddf6cbbb3bf0e5c76b342/user_uploaded_data/logos/Logo_zqPmKJ7X.png",
+    role: "Java Developer",
+    company: "Decipher Zone Softwares",
+    date: "Jul 2023 - Persent",
+    desc: "Java Developer | Skilled in Building Scalable Applications | Problem-Solver Experienced Java Developer with a strong foundation in designing, developing, and maintaining robust, scalable, and high-performance applications. Proficient in Java SE/EE, Spring Framework, Hibernate, and RESTful APIs. Adept at working in agile environments, collaborating across teams, and delivering quality solutions within deadlines. Passionate about solving complex problems, optimizing code, and exploring new technologies like GraphQL, microservices architecture, and DevOps tools.",
+    skills: [
+      "Java",
+      "Kotlin",
+      "Spring Boot",
+      "React JS",
+      "MicroServices",
+      "MySql",
+      "AWS S3",
+      "SQS",
+      "Keycloak",
+      "Cognito",
+      "Docker",
+      "Kubernetes",
+      "Unit & Integration Testing",
+      "Datadog",
+    ],
+  },
+  {
+    id: 1,
+    img: "https://media.licdn.com/dms/image/v2/C510BAQGYgGlur1LLCA/company-logo_200_200/company-logo_200_200/0/1631414394343/divergent_software_labs_pvt_ltd_logo?e=2147483647&v=beta&t=qa8mHUmiAkEswBQlwn2tN1n3vktR7hu9IdpdkbnZ_4c",
+    role: "Junior Java Developer",
+    company: "Divergent Software labs PVT LTD",
+    date: "May 2022 - July 2023",
+    desc: "As a Junior Java Developer at Divergent Soft, I contribute to the development and maintenance of scalable, high-performance applications using Java and Spring Boot. I collaborate with cross-functional teams to build efficient backend solutions, integrate with databases, and optimize system performance.",
+    skills: [
+      "Java",
+      "Spring Boot",
+      "Spring Data JPA",
+      "Postgresql",
+      "Git",
+      "React.js",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Material UI",
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://www.clipartmax.com/png/middle/351-3519497_svce-swami-vivekanand-college-indore.png",
+    school: "Swami Vivekanand College of Engineering, Indore",
+    date: "Jul 2017 - Oct 2021",
+    grade: "7.20 CGPA",
+    desc: "I completed my Bachelor’s in Mechanical Engineering at Swami Vivekanand College of Engineering, Indore.",
+    degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+  },
+  {
+    id: 1,
+    school: "Narmada Public School, Selda.",
+    date: "2016-2017",
+    grade: "73.6%",
+    desc: "I completed my class 12 high school education at Narmada Public School in Selda.",
+    degree: "12th, Mathematics",
+  },
+  {
+    id: 2,
+    school: "Narmada Public School, Selda",
+    date: "2014 - 2015",
+    grade: "70.83%",
+    desc: "I completed my Class 10 education at Narmada Public School in Selda.",
+    degree: "10th MP Board",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Let She GO",
+    date: "Aug 2023 - Present",
+    image:
+      "https://mir-s3-cdn-cf.behance.net/projects/404/702fbe43786149.57fcd9caab210.jpeg",
+    description:
+      "LetSheGo Group, based in Botswana, offers inclusive financial solutions across 11 African countries, helping underserved communities access financial services.",
+    category: "web app",
+    webapp: "https://www.letsgo.letshego.com/",
+    tags: [
+      "Java",
+      "Kotlin",
+      "Spring Boot",
+      "MicroServices",
+      "AWS S3",
+      "SQS",
+      "Keycloak",
+      "Cognito",
+      "Docker",
+      "Kubernetes",
+      "JUnit",
+      "Datadog",
+    ],
+  },
+  {
+    id: 2,
+    title: "Fasciculus CRM",
+    date: "Jun 2022 - Jun 2023",
+    image: "https://fasiculus.com/wp-content/uploads/2019/12/logo.png",
+    bgColor: "#000000",
+    description:
+      "Fasiculus CRM is a powerful and scalable Customer Relationship Management (CRM) system designed to streamline business operations, enhance customer interactions, and optimize sales processes. It provides a centralized platform for managing customer data, tracking interactions, automating workflows, and improving overall customer engagement.",
+    category: "web app",
+    webapp: "https://funqid.com/#/",
+    tags: [
+      "Java",
+      "Spring Boot",
+      "Spring Data JPA",
+      "Postgresql",
+      "Git",
+      "React.js",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Material UI",
+    ],
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
